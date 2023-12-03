5a25fdab0a2d9855062af3b1218f3bb54fc8486e
UmbertoSerino <serumberto@gmail.com>
2023-12-03 17:07:59 +0100

    complete exercise social post

---
+++ main.js
@@ -0,0 +1,141 @@
+/* Descrizione
+Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+Milestone 1 - Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+id del post, numero progressivo da 1 a n
+nome autore,
+foto autore,
+data in formato americano (mm-gg-yyyy),
+testo del post,
+immagine (non tutti i post devono avere una immagine),
+numero di likes.
+Non è necessario creare date casuali
+Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
+Milestone 2 -
+Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+Milestone 3 -
+Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+*/
+
+
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "created": "2021-09-03"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "created": "2021-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2021-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+        "created": "2021-03-05"
+    }
+];
+
+const containerPost = document.querySelector("div#container");
+
+posts.forEach(post => {
+    const postToBeInserted = createdSlide(post);
+    containerPost.innerHTML += postToBeInserted;
+});
+
+// ===========================================
+
+const likeButtonsList = containerPost.querySelectorAll('a.like-button.js-like-button');
+const likeCountersList = containerPost.querySelectorAll('strong.js-likes-counter');
+
+likeButtonsList.forEach((likeButton, index) => {
+    likeButton.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const likeCounter = likeCountersList[index];
+        const currentLikes = parseInt(likeCounter.innerHTML, 10);
+
+        if (likeButton.classList.contains('like-button--liked')) {
+            likeButton.classList.remove('like-button--liked');
+            likeCounter.innerHTML = currentLikes - 1;
+        } else {
+            likeButton.classList.add('like-button--liked');
+            likeCounter.innerHTML = currentLikes + 1;
+        }
+    });
+});
+
+// ====================== Function ===================
+function createdSlide(post) {
+    return `<div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${post.author.name}</div>
+                <div class="post-meta__time">${post.created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${post.content}</div>
+    <div class="post__image">
+        <img src="${post.media}" alt="${post.author.name}">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <strong id="like-counter-${post.id}" data-postid="${post.id}" class="js-likes-counter">${post.likes}</strong> persone
+            </div>
+        </div> 
+    </div>            
+</div>`;
+}
+
+
